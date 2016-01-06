84cb9eedf9c4f442565914a0523148a9af332c31
Charles Fulton <fultonc@lafayette.edu>
2016-01-06 15:55:19 -0800

    Fix numbering warnings

--- version.php
+++ version.php
@@ -20,8 +20,8 @@
  */
 
 $plugin->component = 'local_remote_backup_provider';
-$plugin->cron      = '300';
+$plugin->cron      = 300;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = 'Alpha for 2.8+';
-$plugin->requires  = '2014111000';
-$plugin->version   = '2015080800';
+$plugin->requires  = 2014111000;
+$plugin->version   = 2015080800;
