828a6133239f45c7bd9609f4ecfb0303f97afb18
Charles Fulton <fultonc@lafayette.edu>
2017-10-26 10:51:41 -0400

    Add internal checks

--- db/services.php
+++ db/services.php
@@ -19,6 +19,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 $functions = array(
     'local_remote_backup_provider_find_courses' => array(
          'classname' => 'local_remote_backup_provider_external',
