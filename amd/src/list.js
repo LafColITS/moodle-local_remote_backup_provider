35fdfdb8c66ff391091d104263f6942ae29832f9
georgmaisser <gem@gmx.at>
2020-05-28 16:21:17 +0200

    Add Javascript Ajax call on delete button

--- amd/src/list.js
+++ amd/src/list.js
@@ -6,13 +6,33 @@
  * @copyright  2020 Wunderbyte GmbH
  */
 
-define(['jquery'], function ($) {
+define(['jquery', 'core/ajax', 'core/notification'], function ($, ajax, notification) {
 
     return {
         init: function () {
-            //test
-            $(".form-control").change(function ($value) {
-                alert($value);
+            //we add listener to dropdown function
+            $(".rbp_dropdown").change(function () {
+
+                //collect all the values
+                var firstusername = $('#rbp_userrow_' + this.id + '_username').text();
+
+                alert("change " + this.id + " - " + firstusername);
+            });
+
+            $(".rbp_delete").click(function () {
+
+                var id = this.id;
+                ajax.call([{
+                    methodname: "local_remote_backup_provider_delete_user_entry_from_backup",
+                    args: {'id': this.id},
+                    done: function () {
+                        $('#rbp_userrow_' + id).remove();
+                        alert("we have deleted "+id);
+                    },
+                    fail: notification.exception
+                }]);
+
+
             });
         }
     };
