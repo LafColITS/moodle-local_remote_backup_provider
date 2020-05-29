34e88af2e6540c9be8234a4b916735a42ff7f260
georgmaisser <gem@gmx.at>
2020-05-29 12:06:16 +0200

    Debug Javascript

--- amd/src/list.js
+++ amd/src/list.js
@@ -82,61 +82,72 @@ define(['jquery', 'core/ajax', 'core/notification'], function ($, ajax, notifica
 
                 if (optionaluserelement) {
                     //we only do the work if we selected somebody
+                    var optionaluser = {
+                        'id':$(this).val(),
+                        'username':optionaluserelement.data('username'),
+                        'firstname':optionaluserelement.data('firstname'),
+                        'lastname':optionaluserelement.data('lastname'),
+                        'useremail':optionaluserelement.data('useremail')
+                    };
+
+                    var firstuserelement = $('#rbp_userrow_'+this.id);
+                    var firstuser = {
+                        'id':this.id,
+                        'username':firstuserelement.data('username'),
+                        'firstname':firstuserelement.data('firstname'),
+                        'lastname':firstuserelement.data('lastname'),
+                        'useremail':firstuserelement.data('useremail')
+                    };
+
+                    optionaluserelement.text(firstuser['username']
+                        +" "+firstuser['firstname']
+                        +" "+firstuser['lastname']
+                        +" "+firstuser['useremail']);
+
+                    // Now we change the appearance.
+                    firstuserelement.find('td').eq(0).html(optionaluser['username']);
+                    firstuserelement.find('td').eq(1).html(optionaluser['firstname']);
+                    firstuserelement.find('td').eq(2).html(optionaluser['lastname']);
+                    firstuserelement.find('td').eq(3).html(optionaluser['useremail']);
+
+                    // And now we switch the database.
+                    firstuserelement.data('username', optionaluser['username']);
+                    firstuserelement.data('firstname', optionaluser['firstname']);
+                    firstuserelement.data('lastname', optionaluser['lastname']);
+                    firstuserelement.data('useremail', optionaluser['useremail']);
+
+                    optionaluserelement.data('username', firstuser['username']);
+                    optionaluserelement.data('firstname', firstuser['firstname']);
+                    optionaluserelement.data('lastname', firstuser['lastname']);
+                    optionaluserelement.data('useremail', firstuser['useremail']);
+
+                    var blankelement = $(this).find('.blankvalue');
+                    blankelement.text('Merged with existing user');
+                    blankelement.attr('selected','selected');
+                    // We have to remove the selected Attribute right away to make it work more than once
+                    blankelement.removeAttr('selected');
                 }
                 else {
                     alert("we drew blank");
                 }
-                var optionaluser = {
-                    'id':$(this).val(),
-                    'username':optionaluserelement.data('username'),
-                    'firstname':optionaluserelement.data('firstname'),
-                    'lastname':optionaluserelement.data('lastname'),
-                    'useremail':optionaluserelement.data('useremail')
-                };
-
-                var firstuserelement = $('#rbp_userrow_'+this.id);
-                var firstuser = {
-                    'id':this.id,
-                    'username':firstuserelement.data('username'),
-                    'firstname':firstuserelement.data('firstname'),
-                    'lastname':firstuserelement.data('lastname'),
-                    'useremail':firstuserelement.data('useremail')
-                };
-
-                //now we change the appearance
-                firstuserelement.find('td').eq(0).html(optionaluser['username']);
-                firstuserelement.find('td').eq(1).html(optionaluser['firstname']);
-                firstuserelement.find('td').eq(2).html(optionaluser['lastname']);
-                firstuserelement.find('td').eq(3).html(optionaluser['useremail']);
-
-                firstuserelement.data('username', optionaluser['username']);
-                firstuserelement.data('firstname', optionaluser['firstname']);
-                firstuserelement.data('lastname', optionaluser['lastname']);
-                firstuserelement.data('useremail', optionaluser['useremail']);
-
-                optionaluserelement.data('username', firstuser['username']);
-                optionaluserelement.data('firstname', firstuser['firstname']);
-                optionaluserelement.data('lastname', firstuser['lastname']);
-                optionaluserelement.data('useremail', firstuser['useremail']);
-
-                optionaluserelement.text(firstuser['username']+" "+firstuser['useremail']);
-
-                var blankelement = $(this).find('.blankvalue');
-                blankelement.text('Merged with existing user');
-                blankelement.attr('selected','selected');
-                // We have to remove the selected Attribute right away to make it work more than once
-                blankelement.removeAttr('selected');
             });
 
             $(".rbp_delete").click(function () {
 
                 var restoreid = $('#tabletoexport').data('restoreid');
 
+                var link = $('#continue').attr("href");
+                if (link.indexOf("&pathnamehash") != -1) {
+                    link = link.substring(0, link.indexOf("&pathnamehash"));
+                    link = link+"&filename=updated_backup.mbz";
+                }
+
                 var id = this.id;
                 ajax.call([{
                     methodname: "local_remote_backup_provider_delete_user_entry_from_backup",
                     args: {'id': this.id, 'restoreid':restoreid},
                     done: function () {
+                        $('#continue').attr("href", link);
                         $('#rbp_userrow_' + id).remove();
                     },
                     fail: notification.exception
